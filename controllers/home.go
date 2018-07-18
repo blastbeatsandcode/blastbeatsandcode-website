9f61f7738ac43be5bd5fda78860a8e7683b57b0e
blastbeatsandcode <alexsilcott@gmail.com>
2018-07-17 23:09:53 -0400

    Added some texture to the home page and removed a bit of code from social.gohtml. The home page now shows the last blog post and has a placeholder for plugging current projects into.

--- controllers/home.go
+++ controllers/home.go
@@ -2,10 +2,29 @@ package controllers
 
 import (
 	"net/http"
+
+	"github.com/blastbeatsandcode/blastbeatsandcode-website/models"
+	"github.com/blastbeatsandcode/blastbeatsandcode-website/utils"
+	"github.com/jinzhu/gorm"
 )
 
 /* HomeHandler serves the index page */
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Get("index").ExecuteTemplate(w, "base-tpl", nil)
+	db := utils.GetDB()
+	m := getLatestPost(db)
+
+	err := tpl.Get("index").ExecuteTemplate(w, "base-tpl", m)
 	checkErr(err)
+}
+
+func getLatestPost(db *gorm.DB) map[string]interface{} {
+	post := []models.BlogPost{}
+
+	db.Last(&post)
+
+	m := map[string]interface{}{
+		"NewestPost": post,
+	}
+
+	return m
 }
